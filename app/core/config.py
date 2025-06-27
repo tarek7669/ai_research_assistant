f7b395351d509ee75ad9f6934cb8b67ad18cc82d
Tarek Ashraf <tashraf154@gmail.com>
2025-06-27 17:20:30 +0300

    Add QAEngine with Hugging Face integration
    Introduces a QAEngine class in app/services/qa_engine.py that loads a Hugging Face LLM (defaulting to microsoft/phi-2), authenticates using a token, and provides a generate_answer method. Updates config to support new environment variables for model name and token. Adds test_qa.py to .gitignore and updates requirements for accelerate.

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Settings:
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
-    LLM_MODEL = os.getenv("LLM_MODEL")
+    HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+    LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME", "microsoft/phi-2")
     USE_FP16 = os.getenv("USE_FP16")
     
 
---
+++ app/services/qa_engine.py
@@ -0,0 +1,59 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers.pipelines import pipeline
+from app.core.config import settings
+import torch
+from typing import List
+
+class QAEngine:
+    def __init__(self):
+
+        print("Trying to Login to Hugging Face")
+
+        assert "phi" in settings.LLM_MODEL_NAME.lower(), f"Wrong model still being used!: {settings.LLM_MODEL_NAME}"
+        # Login to Hugging Face
+        from huggingface_hub import login
+        login(token=settings.HUGGINGFACE_TOKEN)
+
+        print("Initializing QAEngine.")
+        print(f"Loading LLM: {settings.LLM_MODEL_NAME} (FP16={settings.USE_FP16})")
+
+        # Load Model and tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(settings.LLM_MODEL_NAME)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = AutoModelForCausalLM.from_pretrained(
+            settings.LLM_MODEL_NAME,
+            torch_dtype=torch.float16 if settings.USE_FP16 else torch.float32,
+            device_map=None
+        )
+
+        self.generator = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            device=0 if torch.cuda.is_available() else -1,
+        )
+
+    def generate_answer(self, question: str, context_chunks: List[str]) -> str:
+        
+        context = '\n'.join(context_chunks)
+        
+        prompt = f"""
+        You are an expert assistant.
+        You will asnwer the question using only the information in the context below.
+
+        ### Context:
+        {context}
+
+        ### Question:
+        {question}
+
+        ### Answer:
+        """
+
+        response = self.generator(prompt, max_new_tokens=256, do_sample=False)[0]['generated_text'] # type: ignore
+        
+        # Remove the prompt from the answer
+        answer = response.split("### Answer:")[-1].strip() # type: ignore
+
+        return answer
